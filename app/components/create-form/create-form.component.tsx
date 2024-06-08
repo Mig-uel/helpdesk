b9ec57a7a41294599331d1e1c0c7fae15032af24
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-08 16:28:56 -0400

    feat: Update CreateForm component to include form fields for creating new tickets

--- app/components/create-form/create-form.component.tsx
+++ app/components/create-form/create-form.component.tsx
@@ -14,7 +14,42 @@ const CreateForm = () => {
   const [priority, setPriority] = useState<Priority>('low')
   const [isLoading, setIsLoading] = useState<IsLoading>(false)
 
-  return <main></main>
+  return (
+    <form className='w-1/2'>
+      <label>
+        <span>Title:</span>
+        <input
+          required
+          type='text'
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+      </label>
+      <label>
+        <span>Body:</span>
+        <textarea
+          required
+          onChange={(e) => setBody(e.target.value)}
+          value={body}
+        />
+      </label>
+      <label>
+        <span>Priority:</span>
+        <select
+          value={priority}
+          onChange={(e) => setPriority(e.target.value as Priority)}
+        >
+          <option value='low'>Low Priority</option>
+          <option value='medium'>Medium Priority</option>
+          <option value='high'>High Priority</option>
+        </select>
+      </label>
+      <button className='btn-primary' disabled={isLoading}>
+        {isLoading && <span>Adding...</span>}
+        {!isLoading && <span>Add Ticket</span>}
+      </button>
+    </form>
+  )
 }
 
 export default CreateForm
