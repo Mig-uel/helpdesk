df55540d2a18c683ae53d4c72e6da5e583d8c3d6
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-08 14:56:42 -0400

    feat: Add ticket details page component

---
+++ app/tickets/[id]/page.tsx
@@ -0,0 +1,38 @@
+interface TicketDetails {
+  params: {
+    id: string
+  }
+}
+
+const getTicketById = async (id: string): Promise<Ticket> => {
+  const res = await fetch(`http://localhost:4000/tickets/${id}`, {
+    next: {
+      revalidate: 60,
+    },
+  })
+
+  return res.json()
+}
+
+const TicketDetails = async ({ params }: TicketDetails) => {
+  const ticket = await getTicketById(params.id)
+
+  return (
+    <main>
+      <nav>
+        <h2>Ticket Details</h2>
+      </nav>
+
+      <div className='card'>
+        <h3>{ticket.title}</h3>
+        <small>Created by: {ticket.user_email}</small>
+        <p>{ticket.body}</p>
+        <div className={`pill ${ticket.priority}`}>
+          {ticket.priority} priority
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default TicketDetails
