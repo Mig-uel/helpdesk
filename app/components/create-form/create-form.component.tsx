82407176515a6bf586c542bdc9f96b71a0691cca
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-08 16:28:48 -0400

    feat: Update CreateForm component to use next/navigation instead of next/router

--- app/components/create-form/create-form.component.tsx
+++ app/components/create-form/create-form.component.tsx
@@ -1,12 +1,14 @@
 'use client'
 import { useState } from 'react'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 
 type Title = string
 type Body = string
 type IsLoading = boolean
 
 const CreateForm = () => {
+  const router = useRouter()
+
   const [title, setTitle] = useState<Title>('')
   const [body, setBody] = useState<Body>('')
   const [priority, setPriority] = useState<Priority>('low')
