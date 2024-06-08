2ff170817c684ebed9a19d0ea56ac47b6839e440
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-08 14:56:31 -0400

    fix: Correct link href for Tickets in navbar component

--- app/components/navbar/navbar.component.tsx
+++ app/components/navbar/navbar.component.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
       />
       <h1>Helpdesk</h1>
       <Link href='/'>Dashboard</Link>
-      <Link href='tickets'>Tickets</Link>
+      <Link href='/tickets'>Tickets</Link>
     </nav>
   )
 }
