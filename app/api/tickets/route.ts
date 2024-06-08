6fceb04db84c773131c05d7bcb66287638000cea
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-08 19:59:48 -0400

    refactor: Add GET route for fetching tickets from API

---
+++ app/api/tickets/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server'
+
+export const GET = async (): Promise<
+  NextResponse<Ticket[] & { status: number }>
+> => {
+  const res = await fetch('http://localhost:4000/tickets')
+  const tickets = await res.json()
+
+  return NextResponse.json(tickets, {
+    status: 200,
+  })
+}
