42a2dd907780356326f7a01aac88dfaa26ff1e06
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-08 15:22:11 -0400

    refactor: Update NotFound page component to display more specific error message

--- app/tickets/not-found.tsx
+++ app/tickets/not-found.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const NotFound = () => {
   return (
     <main className='text-center'>
-      <h2 className='text-3xl'>There was a problem...</h2>
+      <h2 className='text-3xl'>Unknown ticket...</h2>
       <p>We could not find the ticket you were looking for.</p>
       <p>
         Go back to all <Link href='/tickets'>Tickets</Link>
