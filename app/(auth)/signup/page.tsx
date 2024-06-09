b8b0490825eab97cb6b1e2ca59a73e5d46b2a684
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-08 22:13:04 -0400

    chore: Add AuthForm component to signup page

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,7 +1,24 @@
+import AuthForm from '@/app/components/auth-form/auth-form.component'
+
+type Email = string
+type Password = string
+
 const SignUp = () => {
+  const handleSignUpSubmit = async (
+    e: React.FormEvent<HTMLFormElement>,
+    email: Email,
+    password: Password
+  ) => {
+    e.preventDefault()
+
+    console.log(email, password)
+  }
+
   return (
     <main>
       <h2 className='text-center'>Sign Up</h2>
+
+      <AuthForm handleAuthSubmit={handleSignUpSubmit} />
     </main>
   )
 }
