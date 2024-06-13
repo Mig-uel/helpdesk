b56be49428e993d3edb2d01795a4d00fbae43b3e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-13 19:47:25 -0400

    feat: Add error handling and form validation to signup page

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,19 +1,40 @@
 'use client'
 
+import { useState } from 'react'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import AuthForm from '@/app/components/auth-form/auth-form.component'
+import { useRouter } from 'next/navigation'
 
 type Email = string
 type Password = string
 
 const SignUp = () => {
+  const router = useRouter()
+  const [formError, setFormError] = useState<string>('')
+
   const handleSignUpSubmit = async (
     e: React.FormEvent<HTMLFormElement>,
     email: Email,
     password: Password
   ) => {
     e.preventDefault()
+    const supabase = createClientComponentClient()
 
-    console.log(email, password)
+    try {
+      const { error } = await supabase.auth.signUp({
+        email,
+        password,
+        options: {
+          emailRedirectTo: `${location.origin}/api/auth/callback`,
+        },
+      })
+
+      if (error) throw new Error(error.message)
+
+      router.push('/verify')
+    } catch (error: any) {
+      setFormError(error.message)
+    }
   }
 
   return (
@@ -21,6 +42,7 @@ const SignUp = () => {
       <h2 className='text-center'>Sign Up</h2>
 
       <AuthForm handleAuthSubmit={handleSignUpSubmit} />
+      {formError && <div className='error'>{formError}</div>}
     </main>
   )
 }
