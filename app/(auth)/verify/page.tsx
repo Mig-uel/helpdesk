00892782b12c434946730720f353ce8c41adbe38
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-13 19:47:30 -0400

    refactor: Add verification page for email confirmation

---
+++ app/(auth)/verify/page.tsx
@@ -0,0 +1,13 @@
+const Verify = () => {
+  return (
+    <main className='text-center'>
+      <h2>Thanks for registering!</h2>
+      <p>
+        Complete your sign up by verifying your email address. Please click on
+        the verification link sent to your email.
+      </p>
+    </main>
+  )
+}
+
+export default Verify
