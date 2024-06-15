a009d9a6fbb0b8071307760ee858a3b1fd3362fd
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-15 18:35:12 -0400

    refactor: Update ticket list component to fetch data from Supabase

--- app/(dashboard)/tickets/ticket-list.tickets.tsx
+++ app/(dashboard)/tickets/ticket-list.tickets.tsx
@@ -1,34 +1,39 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import Link from 'next/link'
+import { cookies } from 'next/headers'
+import { PostgrestError } from '@supabase/supabase-js'
 
-const getTickets = async (): Promise<Ticket[]> => {
-  const res = await fetch('http://localhost:4000/tickets', {
-    next: {
-      revalidate: 0, // use 0 to opt out of using cache
-    },
-  })
-  return res.json()
+// because its a server component, no need to create an api route and just fetch straight from component
+
+const getTickets = async (): Promise<{
+  data: Ticket[] | null
+  error: PostgrestError | null
+}> => {
+  const supabase = createServerComponentClient({ cookies })
+  const { data, error } = await supabase.from('tickets').select()
+
+  return { data, error }
 }
 
 const TicketList = async () => {
-  const tickets = await getTickets()
+  const { data: tickets, error } = await getTickets()
 
-  return (
-    <>
-      {tickets.map((ticket) => (
-        <div key={ticket.id} className='card my-5'>
-          <Link href={`/tickets/${ticket.id}`}>
-            <h3>{ticket.title}</h3>
-            <p>{ticket.body.slice(0, 200)}...</p>
-            <div className={`pill ${ticket.priority}`}>
-              {ticket.priority} priority
-            </div>
-          </Link>
-        </div>
-      ))}
-      {tickets.length === 0 && (
-        <p className='text-center'>There are no open tickets...</p>
-      )}
-    </>
+  return tickets ? (
+    tickets?.map((ticket) => (
+      <div key={ticket.id} className='card my-5'>
+        <Link href={`/tickets/${ticket.id}`}>
+          <h3>{ticket.title}</h3>
+          <p>{ticket.body.slice(0, 200)}...</p>
+          <div className={`pill ${ticket.priority}`}>
+            {ticket.priority} priority
+          </div>
+        </Link>
+      </div>
+    ))
+  ) : (
+    <p className='text-center'>
+      {error ? 'Something went wrong...' : 'There are no open tickets...'}
+    </p>
   )
 }
 
