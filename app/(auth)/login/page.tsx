ff4977327e8239649dd016f9c3b0aa8c66c77765
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-15 17:24:40 -0400

    refactor: Update login page to handle form submission and display form error message

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,17 +1,42 @@
 'use client'
 
+// libs
+import { useState } from 'react'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { useRouter } from 'next/navigation'
+
+// components
 import AuthForm from '@/app/components/auth-form/auth-form.component'
 
 type Email = string
 type Password = string
 
 const Login = () => {
+  const router = useRouter()
+  const [formError, setFormError] = useState<string>('')
+
   const handleLoginSubmit = async (
     e: React.FormEvent<HTMLFormElement>,
     email: Email,
     password: Password
   ) => {
     e.preventDefault()
+    setFormError('')
+
+    const supabase = createClientComponentClient()
+
+    try {
+      const { error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      })
+
+      if (error) throw new Error(error.message)
+
+      router.push('/')
+    } catch (error) {
+      setFormError((error as Error).message || 'Please try again')
+    }
   }
 
   return (
@@ -19,6 +44,7 @@ const Login = () => {
       <h2 className='text-center'>Login</h2>
 
       <AuthForm handleAuthSubmit={handleLoginSubmit} />
+      {formError && <div className='error'>{formError}</div>}
     </main>
   )
 }
