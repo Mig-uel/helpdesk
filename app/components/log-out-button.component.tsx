17a2390d009117701908cc96928e3136ad56aa37
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-15 17:12:55 -0400

    refactor: Add LogoutButton component for logging out

---
+++ app/components/log-out-button.component.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { useRouter } from 'next/navigation'
+
+const LogoutButton = ({ text }: { text: string }) => {
+  const router = useRouter()
+
+  const logoutHandler = async () => {
+    const supabase = createClientComponentClient()
+
+    try {
+      const { error } = await supabase.auth.signOut()
+
+      if (error) throw new Error(error.message)
+
+      router.push('/login')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <button className='btn-primary' onClick={logoutHandler}>
+      {text}
+    </button>
+  )
+}
+
+export default LogoutButton
