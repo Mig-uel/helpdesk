cb514a37ef56b9bcac7797225bdde229ba2e9bbb
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-14 20:18:40 -0400

    refactor: Update dashboard layout to include user information in the navbar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,13 +1,22 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+// components
 import Navbar from '../components/navbar/navbar.component'
 
-const DashboardLayout = ({
+const DashboardLayout = async ({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) => {
+  const supabase = createServerComponentClient({ cookies })
+  const { data } = await supabase.auth.getSession()
+
+  // console.log(data)
+
   return (
     <>
-      <Navbar />
+      <Navbar user={data?.session?.user} />
       {children}
     </>
   )
