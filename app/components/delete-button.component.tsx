d22918b70694b2d02ed8ce55bd7acfd2a31646cb
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-16 20:01:15 -0400

    refactor: Update DeleteButton component to use Supabase for ticket deletion

--- app/components/delete-button.component.tsx
+++ app/components/delete-button.component.tsx
@@ -1,17 +1,36 @@
 'use client'
 
+import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 import { TiDelete } from 'react-icons/ti'
 
 const DeleteButton = ({ id }: { id: string | undefined }) => {
+  const router = useRouter()
   const [isLoading, setIsLoading] = useState<boolean>(false)
 
   const deleteHandler = async () => {
     setIsLoading((prev) => !prev)
 
-    alert('deleting id: ' + id)
+    try {
+      const res = await fetch(`http://localhost:3000/api/tickets/${id}`, {
+        method: 'DELETE',
+      })
 
-    setIsLoading((prev) => !prev)
+      const { error } = await res.json()
+
+      if (error)
+        throw new Error(
+          error?.message ||
+            'An error occurred while trying to delete this ticket. Please try again...'
+        )
+
+      setIsLoading((prev) => !prev)
+      router.refresh()
+      router.push('/tickets')
+    } catch (error) {
+      setIsLoading((prev) => !prev)
+      alert((error as Error).message)
+    }
   }
 
   return (
