7911d4e19978951fcbe913ce97fa347fddbe0d3d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-16 22:52:33 -0400

    refactor: Improve form submission handling in addTicket action

--- app/(dashboard)/tickets/actions.ts
+++ app/(dashboard)/tickets/actions.ts
@@ -10,26 +10,20 @@ export const addTicket = async (data: FormData) => {
   const ticket = Object.fromEntries(data)
   const supabase = createServerActionClient({ cookies })
 
-  try {
-    const {
-      data: { session },
-    } = await supabase.auth.getSession()
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
 
-    // insert data
-    const { data, error } = await supabase
-      .from('tickets')
-      .insert({ ...ticket, user_email: session?.user.email })
-      .select()
+  // insert data
+  const { error } = await supabase
+    .from('tickets')
+    .insert({ ...ticket, user_email: session?.user.email })
 
-    if (data === null) {
-      throw new Error(error?.message)
-    }
-
-    revalidatePath('/tickets')
-  } catch (error) {
-    return { message: (error as Error).message }
+  if (error) {
+    throw new Error('Something went wrong :X')
   }
 
   // redirect user
+  revalidatePath('/tickets')
   redirect('/tickets')
 }
