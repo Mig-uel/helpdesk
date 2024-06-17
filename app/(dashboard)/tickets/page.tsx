e2f13f3c2204db2cc7bad02b7f5d31ec65465ea5
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-16 20:27:59 -0400

    refactor: Add "New Ticket" button to Tickets page

--- app/(dashboard)/tickets/page.tsx
+++ app/(dashboard)/tickets/page.tsx
@@ -2,6 +2,7 @@ import { Suspense } from 'react'
 import { Metadata } from 'next'
 import TicketList from './ticket-list.tickets'
 import Loading from '../loading'
+import Link from 'next/link'
 
 export const metadata: Metadata = {
   title: 'Helpdesk | Tickets',
@@ -15,6 +16,9 @@ const Tickets = () => {
           <h2>Tickets</h2>
           <p>
             <small>Currently open tickets</small>
+            <Link href='/tickets/create' className='m1-auto'>
+              <button className='btn-primary'>New Ticket</button>
+            </Link>
           </p>
         </div>
       </nav>
