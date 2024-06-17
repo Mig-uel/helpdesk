4b3eaeb77f786641c8e0ed880be1a8e82dac194b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-16 23:28:13 -0400

    refactor: Update DeleteButton component to use useTransition hook for deleteTicket action

--- app/components/delete-button.component.tsx
+++ app/components/delete-button.component.tsx
@@ -1,47 +1,20 @@
 'use client'
-
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
+import { useTransition } from 'react'
 import { TiDelete } from 'react-icons/ti'
+import { deleteTicket } from '../(dashboard)/tickets/actions'
 
 const DeleteButton = ({ id }: { id: string | undefined }) => {
-  const router = useRouter()
-  const [isLoading, setIsLoading] = useState<boolean>(false)
-
-  const deleteHandler = async () => {
-    setIsLoading((prev) => !prev)
-
-    try {
-      const res = await fetch(`http://localhost:3000/api/tickets/${id}`, {
-        method: 'DELETE',
-      })
-
-      const { error } = await res.json()
-
-      if (error)
-        throw new Error(
-          error?.message ||
-            'An error occurred while trying to delete this ticket. Please try again...'
-        )
-
-      setIsLoading((prev) => !prev)
-      router.refresh()
-      router.push('/tickets')
-    } catch (error) {
-      setIsLoading((prev) => !prev)
-      alert((error as Error).message)
-    }
-  }
+  const [isPending, startTransition] = useTransition()
 
   return (
     <button
       className='btn-primary'
-      onClick={deleteHandler}
-      disabled={isLoading}
+      onClick={() => startTransition(deleteTicket(id) as any)}
+      disabled={isPending}
     >
       <>
         <TiDelete />
-        {isLoading ? 'Deleting...' : 'Delete Ticket'}
+        {isPending ? 'Deleting...' : 'Delete Ticket'}
       </>
     </button>
   )
